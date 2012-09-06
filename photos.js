b9c1944bdeca86658d421bb51a344c98e637d51e
buliq <buliq1847@gmail.com>
2012-09-06 16:40:46 +0200

    added Fav button

--- photos.js
+++ photos.js
@@ -66,10 +66,89 @@ var setupPhotos = (function ($) {
         return function (img) {
             var elm = document.createElement('div');
             elm.className = 'photo';
+			//add fave
+			elm.appendChild(addFavButton(img.src));
             elm.appendChild(img);
             holder.appendChild(elm);
         };
     }
+	
+	function addFavButton(img) {
+		var el = document.createElement('i'); //this is our favourite button
+		el.className = isFav(img) ? 'icon-heart' : 'icon-heart-empty'; // is it our favourite?
+		el.onclick = function() {
+			img = this.parentNode.childNodes[1]; // we are looking for img that is in the same container( the same parent)
+			updateFav(img.src);
+			updateState(img, this);
+		};
+		// mouseover and mouseout are not required, just looks better :)
+		el.onmouseover = function() { 
+			if (this.className == 'icon-heart') {
+				this.className = 'icon-heart-empty';
+			}
+			else if (this.className == 'icon-heart-empty') {
+				this.className = 'icon-heart';
+			}
+		};
+		el.onmouseout = function() {
+			img = this.parentNode.childNodes[1];
+			updateState(img, this); // be sure to update to good state
+		};
+		return el;
+	}
+	
+	function updateFav(img) {
+		//we just need img src to add it into cookie
+		//state is choosen here, it reverses actual state and update cookie
+		if (isFav(img)) {
+			val = isFav();
+			for (i in val) {
+				if (val[i] == img) {
+					delete val[i];
+				}
+			}
+		}
+		else {
+			val = isFav();
+			val[val.length] = img;
+		}
+		
+		document.cookie = 'fav='+escape(val.join())+";";
+	}
+	
+	function isFav(img) {
+		//if our cookie not exists, add empty
+		if (!document.cookie.length || document.cookie.indexOf('fav') == -1) {
+			document.cookie = "fav=;";
+		}
+		//we know how we named cookie, so just find that one
+		value = document.cookie.split('fav')[1].split(';')[0].split('=')[1];
+		values = unescape(value).split(',');
+		
+		//this could be helpful, somewhere, but not now
+		if (img == null) {
+			return values;
+		}
+		else {
+			//true, if img is favourite
+			for (i in values) {
+				if (values[i] == img) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	//this one just updates class of fav button
+	function updateState(img, el) {
+		if (isFav(img.src)) {
+			el.className = 'icon-heart';
+		}
+		else {
+			el.className = 'icon-heart-empty';
+		}
+	}
 
     // ----
     
